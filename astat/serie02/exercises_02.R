820efb33cbca63251ba9f9bbe2f8bc6cff6a627c
Timon Schmid <timon.schmid@bluewin.ch>
2023-10-30 20:12:43 +0100

    solve exercises 02

--- astat/serie02/exercises_02.R
+++ astat/serie02/exercises_02.R
@@ -37,10 +37,53 @@ winner.sd <- sqrt(winner.var)
 noten <- c(4.2, 2.3, 5.6, 4.5, 4.8, 3.9, 5.9, 2.4, 5.9, 6, 4, 3.7, 5, 5.2, 4.5, 3.6, 5, 6, 2.8, 3.3, 5.5, 4.2, 4.9, 5.1)
 
 # a
-noten_2 <- c(1,2.3,5.6,4.5,4.8,3.9,5.9, 2.4,5.9,6,4,3.7,5,5.2,1,3.6,5,6,
+noten_2 <- c(1,2.3,5.6,4.5,4.8,3.9,5.9,2.4,5.9,6,4,3.7,5,5.2,1,3.6,5,6,
              2.8,3.3,5.5,1,4.9,5.1)
 
 # b
 boxplot(noten, noten_2,
-        main = "Boxplot",
         col = c("orange", "lightblue"))
+
+# Aufgabe 2.3
+# a
+mannfrau <- read.csv("serie02/mannfrau.csv")
+head(mannfrau)
+
+# b
+diff_alter <- mannfrau$alter.mann - mannfrau$alter.frau
+
+boxplot(diff_alter,
+        main="Altersdifferenz Ehemaenner & Ehefrauen",
+        col="orange")
+
+# Aufgabe 2.4
+# a
+tapply(InsectSprays[, "count"], InsectSprays[, "spray"], FUN = mean)
+
+# b
+tapply(InsectSprays$count, InsectSprays$spray, mean)
+
+# c
+boxplot(count ~ spray,
+        data = InsectSprays,
+        col=c("orange", "blue", "darkseagreen", "deeppink",
+              "brown", "aquamarine")
+)
+
+# Aufgabe 2.5
+diet <- read.csv("serie02/Diet.csv")
+head(diet)
+
+diet$weight.loss <- diet$weight6weeks - diet$pre.weight
+head(diet)
+
+# a
+tapply(diet[, "weight.loss"], diet[, "Diet"], FUN = mean)
+
+# b
+tapply(diet$weight.loss, diet$Diet, mean)
+
+# c
+boxplot(weight.loss ~ Diet,
+        data = diet,
+        col=c("orange", "darkseagreen", "aquamarine"))
